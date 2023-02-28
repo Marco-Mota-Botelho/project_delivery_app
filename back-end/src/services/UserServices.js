9b57a7516961aebcd7477aa60a9c1ad584b4d2b8
Marcos Vinicius <vinnyzsouza@gmail.com>
2023-02-27 22:45:10 -0300

    :bug: fix error lint

--- back-end/src/services/UserServices.js
+++ back-end/src/services/UserServices.js
@@ -1,6 +1,6 @@
 const { User } = require('../database/models');
 
-const login = async (email, password) => {
+const login = async (email) => {
   const result = await User.findOne({ where: { email } });
   if (!result) return { status: 404, message: 'Not found' };
   return { status: 404, result };
