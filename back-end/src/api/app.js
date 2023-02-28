37a899111bae1c6aae5bad65e9a8dc5d6b7f5ba0
Marcos Vinicius <vinnyzsouza@gmail.com>
2023-02-28 12:11:41 -0300

    :sparkles: feat add use cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const ErrorHandler = require('../middlewares/ErrorHandler');
 const usersRoutes = require('../routes/users.routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(ErrorHandler.handler);
-app.use('/login', usersRoutes);
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/login', usersRoutes);
 
 module.exports = app;
