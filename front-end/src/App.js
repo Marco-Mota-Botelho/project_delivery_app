5421fe7687d4bf42788a3c06b83a06235fa7e8a8
MarcosFFurtado <marcosfurtado@icloud.com>
2023-02-28 14:39:41 -0300

    feak reqs 6 e 7 register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/" element={ <Navigate replace to="/login" /> } />
+      <Route exact path="/register" element={ <Register /> } />
     </Routes>
   );
 }
