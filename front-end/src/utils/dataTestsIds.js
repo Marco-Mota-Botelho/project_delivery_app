d1252aa0147de3a5209df5f486d69150f89071cd
Marcos Vinicius <vinnyzsouza@gmail.com>
2023-03-02 18:49:27 -0300

    feat add test-id orders

--- front-end/src/utils/dataTestsIds.js
+++ front-end/src/utils/dataTestsIds.js
@@ -1,19 +1,27 @@
 const TEST_ID_CUSTOMER_ORDER_DETAILS = {
-  ORDER_ID: 'customer_order_details__element-order-details-label-order-id',
-  SELLER_NAME: 'customer_order_details__element-order-details-label-seller-name',
-  ORDER_DATE: 'customer_order_details__element-order-details-label-order-date',
-  DELIVERY_STATUS: 'customer_order_details__element-order-details-label-delivery-status-',
-  PRODUCT_INDEX: 'customer_order_details__element-order-table-item-number-',
-  PRODUCT_NAME: 'customer_order_details__element-order-table-name-',
-  PRODUCT_COUNT: 'customer_order_details__element-order-table-quantity-',
-  PRODUCT_PRICE: 'customer_order_details__element-order-table-unit-price-',
-  PRODUCT_TOTAL: 'customer_order_details__element-order-table-sub-total-',
-  TOTAL_PRICE: 'customer_order_details__element-order-total-price',
-  DELIVERY_CHECK: 'customer_order_details__button-delivery-check',
+  ORDER_ID: '_order_details__element-order-details-label-order-id',
+  SELLER_NAME: '_order_details__element-order-details-label-seller-name',
+  ORDER_DATE: '_order_details__element-order-details-label-order-date',
+  DELIVERY_STATUS: '_order_details__element-order-details-label-delivery-status-',
+  PRODUCT_INDEX: '_order_details__element-order-table-item-number-',
+  PRODUCT_NAME: '_order_details__element-order-table-name-',
+  PRODUCT_COUNT: '_order_details__element-order-table-quantity-',
+  PRODUCT_PRICE: '_order_details__element-order-table-unit-price-',
+  PRODUCT_TOTAL: '_order_details__element-order-table-sub-total-',
+  TOTAL_PRICE: '_order_details__element-order-total-price',
+  DELIVERY_CHECK: '_order_details__button-delivery-check',
 };
 
 const TEST_ID_CUSTOMER_ORDER = {
-
+  ORDER_ID: '_orders__element-order-id-',
+  DELIVERY_STATUS: '_orders__element-delivery-status-',
+  ORDER_DATE: '_orders__element-order-date-',
+  PRICE: '_orders__element-card-price-',
 };
 
-export { TEST_ID_CUSTOMER_ORDER_DETAILS, TEST_ID_CUSTOMER_ORDER };
+const TEST_ID_PRODUCTS = {
+  BUTTON_CART: 'customer_products__button-cart',
+  BOTTOM_VALUE: 'customer_products__checkout-bottom-value',
+};
+
+export { TEST_ID_CUSTOMER_ORDER_DETAILS, TEST_ID_CUSTOMER_ORDER, TEST_ID_PRODUCTS };
