59df6487de0165412e3bb6391e382830b06ddc11
Marcos Vinicius <vinnyzsouza@gmail.com>
2023-03-07 12:49:44 -0300

    :recycle: refactor arquivos não usados

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 import App from './App';
 import ThemeProvider from './context/ThemeProvider';
 import GlobalStyled from './styles/GlobalStyles';
